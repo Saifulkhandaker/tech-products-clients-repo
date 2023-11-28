717b9221b5f4da64257e36d1193e60341fc35205
Saifulkhandaker <khsaiful67@gmail.com>
2023-11-28 12:44:41 +0600

    manage coupon page added

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# React + Vite
+# Project : Clima Tech-Hunt
+# Live : 
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# Project Features :
 
-Currently, two official plugins are available:
+- User-Friendly Product Interaction: Users can seamlessly interact with the website by upvoting products and accessing detailed information by clicking on the product name within the product card. The website provides a smooth and intuitive experience for users to engage with the showcased products.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Personalized User Dashboard: This platform offers a personalized user dashboard where individuals can view their profiles, add new products, and manage their listed products. Users have the flexibility to update and delete their products directly from the "My Product" page, enhancing their control over the content they contribute.
+
+- Admin Insights through Dynamic Charts: The admin dashboard provides a comprehensive overview of the platform's performance through dynamic pie charts. Admins can quickly assess the total number of users and products, offering valuable insights into the platform's growth and user engagement.
+
+- Efficient User Management for Admins: In the "Manage Users" section of the admin dashboard, administrators can efficiently handle user accounts. This includes the ability to delete and update user profiles, as well as promote users to admin status. This feature ensures robust control and management capabilities for the platform administrators.
+
+- Responsive Design for Seamless Access: The website boasts a responsive design, ensuring a consistent and enjoyable experience across various devices. Whether accessed from a desktop, tablet, or smartphone, users and administrators can interact with the platform effortlessly, promoting accessibility and usability.
+
--- src/Pages/AdminDashboard/Coupon/Coupon.jsx
+++ src/Pages/AdminDashboard/Coupon/Coupon.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { FaDollarSign, FaEdit, FaTrashAlt } from "react-icons/fa";
+import { RiCoupon2Line } from "react-icons/ri";
 
 const Coupon = () => {
+
+    const [coupon, setCoupon] = useState([]);
+    
+    useEffect(() => {
+        fetch('/public/coupon.json')
+        .then(res => res.json())
+        .then(data => {
+            setCoupon(data)
+        })
+    }, [])
+
     return (
         <div>
-            This is coupon page
+            <div className="border shadow-xl bg-white py-10 px-5 mt-5">
+                <div className="flex justify-between">
+                    <h2 className=" text-xl lg:text-3xl font-medium">Total Coupons: {coupon.length}</h2>
+                </div>
+                {/* items table */}
+                <div className="overflow-x-auto mt-4">
+                    <table className="table">
+                    {/* head */}
+                    <thead className="bg-[#94A3B8] text-white rounded-t-full">
+                        <tr>
+                        <th>#</th>
+                        <th>Coupon Code</th>
+                        <th>Expiry Date</th>
+                        <th>D Amount</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        coupon.map((item, index) => <tr key={item._id}>
+                            <th>
+                            { index+1 }
+                            </th>
+                            <td className='flex items-center mt-3 gap-2 text-red-700 font-medium'><RiCoupon2Line></RiCoupon2Line>{item.coupon_code}</td>
+                            <td className="text-xs md:text-sm lg:text-lg p-0">{item.expiration_date}</td>
+                            <td className="text-xs md:text-sm lg:text-lg p-0 flex items-center font-medium text-green-700"><FaDollarSign/>{item.discount_amount}</td>
+                            <td>
+                                <button>
+                                <FaEdit className="items-center text-4xl text-white bg-[#D1A054] py-2 pl-1 rounded-lg"></FaEdit>
+                                </button> 
+                            </td>
+                            <th>
+                            <button
+                            className="btn btn-ghost text-lg text-white bg-[#B91C1C] "><FaTrashAlt></FaTrashAlt>
+                            </button>
+                            </th>
+                        </tr>)
+                    }
+                        
+                    </tbody>
+                    
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
